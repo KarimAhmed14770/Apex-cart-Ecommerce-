6d11a0082e155bdf5dc7b64944c75f0e747d994a
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-26 11:47:38 +0300

    feat: add JpaSpecificationExecutor for OrderRepository

--- src/main/java/com/Kee/V2C/Repository/OrderRepository.java
+++ src/main/java/com/Kee/V2C/Repository/OrderRepository.java
@@ -5,12 +5,13 @@ import com.Kee.V2C.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrderRepository extends JpaRepository<Order,Long>, JpaSpecificationExecutor<Order> {
 
     Page<Order> findByCustomerId(Long id, Pageable pageable);
 
