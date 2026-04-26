a77670be26155f4528c9187e4943e905c5e27a58
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-26 11:48:31 +0300

    feat: Add search functionalities for orders

--- src/main/java/com/Kee/V2C/service/OrderService.java
+++ src/main/java/com/Kee/V2C/service/OrderService.java
@@ -1,11 +1,20 @@
 package com.Kee.V2C.service;
 
 import com.Kee.V2C.dto.customer.CheckOutRequest;
+import com.Kee.V2C.dto.customer.OrderResponse;
 import com.Kee.V2C.entity.CartItem;
 import com.Kee.V2C.entity.Order;
+import com.Kee.V2C.enums.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface OrderService {
     Order convertCartToOrder(CheckOutRequest checkOutRequest, List<CartItem> cart);
+    OrderResponse getOrderById(Long id);
+    Page<OrderResponse> getAllOrders(Pageable page);
+    Page<OrderResponse> getOrderByAttributes(Pageable page, BigDecimal lowerRange, BigDecimal higherRange,
+                                     String address, OrderStatus status);
 }
--- src/main/java/com/Kee/V2C/service/OrderServiceImpl.java
+++ src/main/java/com/Kee/V2C/service/OrderServiceImpl.java
@@ -1,14 +1,22 @@
 package com.Kee.V2C.service;
 
 import com.Kee.V2C.Repository.CustomerRepository;
+import com.Kee.V2C.Repository.OrderRepository;
 import com.Kee.V2C.Repository.VendorRepository;
 import com.Kee.V2C.dto.customer.CheckOutRequest;
+import com.Kee.V2C.dto.customer.OrderResponse;
 import com.Kee.V2C.entity.*;
 import com.Kee.V2C.enums.OrderStatus;
 import com.Kee.V2C.exception.ResourceNotFoundException;
 import com.Kee.V2C.service.Customer.CustomerService;
+import com.Kee.V2C.specifications.CustomerSpecs;
+import com.Kee.V2C.specifications.OrderSpecs;
 import com.Kee.V2C.utils.SecurityUtil;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,12 +31,15 @@ public class OrderServiceImpl implements OrderService{
     private final VendorRepository vendorRepository;
     private final CustomerRepository customerRepository;
     private final SecurityUtil securityUtil;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public OrderServiceImpl(VendorRepository vendorRepository,CustomerRepository customerRepository,SecurityUtil securityUtil){
+    public OrderServiceImpl(VendorRepository vendorRepository,CustomerRepository customerRepository,
+                            SecurityUtil securityUtil,OrderRepository orderRepository){
         this.vendorRepository=vendorRepository;
             this.customerRepository=customerRepository;
             this.securityUtil=securityUtil;
+            this.orderRepository=orderRepository;
     }
 
     @Override
@@ -76,5 +87,49 @@ public class OrderServiceImpl implements OrderService{
             order.setShippingAddress(customer.getShippingAddress());
         }
         return order;
+    }
+
+    @Override
+    public OrderResponse getOrderById(Long id){
+        Long customerId=securityUtil.getCurrentUserId();
+        Order order=orderRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("order with id: "+id+" not found"));
+        if(!order.getCustomer().getId().equals(customerId)){
+            throw new ResourceNotFoundException("you don't have order with id: "+id);
+        }
+        return convertOrderToDto(order);
+    }
+
+    @Override
+    public Page<OrderResponse> getAllOrders(Pageable page){
+        Long customerId=securityUtil.getCurrentUserId();
+        Page<Order> orders=orderRepository.findByCustomerId(customerId,page);
+        return orders.map(this::convertOrderToDto);
+    }
+
+    @Override
+    public Page<OrderResponse> getOrderByAttributes(Pageable page, BigDecimal lowerRange, BigDecimal higherRange,
+                                     String address, OrderStatus status){
+        Specification<Order> spec = (root, query, cb) -> cb.conjunction();
+        Long customerId=securityUtil.getCurrentUserId();
+        if(customerId!=null) spec=spec.and(OrderSpecs.hasCustomerId(customerId));
+
+        if(lowerRange!=null || higherRange!=null) spec=spec.and(OrderSpecs.hasPriceBetween(lowerRange, higherRange));
+
+        if(address!=null && !address.isEmpty()) spec=spec.and(OrderSpecs.hasAddressContains(address));
+
+        if(status!=null) spec=spec.and(OrderSpecs.hasStatus(status));
+
+        Page<Order> orders=orderRepository.findAll(spec,page);
+        return orders.map(this::convertOrderToDto);
+    }
+
+    private OrderResponse convertOrderToDto(Order order){
+        return new OrderResponse(order.getId(),
+                order.getShippingAddress(),
+                order.getOrderedAt(),
+                order.getDelivered_at(),
+                order.getTotalPrice(),
+                order.getStatus());
     }
 }
