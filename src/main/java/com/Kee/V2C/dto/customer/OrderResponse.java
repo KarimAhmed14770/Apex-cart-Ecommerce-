7e933759e431dfa81f6df53e2cc4a809ac4f13df
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-26 11:48:11 +0300

    feat: Add orderResponse dto

---
+++ src/main/java/com/Kee/V2C/dto/customer/OrderResponse.java
@@ -0,0 +1,14 @@
+package com.Kee.V2C.dto.customer;
+
+import com.Kee.V2C.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record OrderResponse(Long id,
+                            String shippingAddress,
+                            LocalDateTime orderedAt,
+                            LocalDateTime deliveredAt,
+                            BigDecimal price,
+                            OrderStatus status) {
+}
