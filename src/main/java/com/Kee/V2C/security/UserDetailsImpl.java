4caf6d212d585d70f7d2b42febcd78b701798d7c
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-27 18:40:07 +0300

    refactor: change the isEnabled function to block suspended users

--- src/main/java/com/Kee/V2C/security/UserDetailsImpl.java
+++ src/main/java/com/Kee/V2C/security/UserDetailsImpl.java
@@ -43,5 +43,5 @@ public class UserDetailsImpl implements UserDetails {
         @Override public boolean isAccountNonExpired() { return true; }
         @Override public boolean isAccountNonLocked() { return true; }
         @Override public boolean isCredentialsNonExpired() { return true; }
-        @Override public boolean isEnabled() { return true; }
+        @Override public boolean isEnabled() { return status!=UserStatus.SUSPENDED; }
     }
