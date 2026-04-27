d7ff6c2bbadc0b03182fb68081da3dd5bf2d6cf6
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-27 18:38:44 +0300

    feat: add validation for CheckOutRequest

--- src/main/java/com/Kee/V2C/dto/checkout/CheckOutRequest.java
+++ src/main/java/com/Kee/V2C/dto/checkout/CheckOutRequest.java
@@ -1,5 +1,12 @@
 package com.Kee.V2C.dto.checkout;
 
-public record CheckOutRequest(Boolean deliverToRegisteredAddress,String shippingAddress) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CheckOutRequest(@NotNull(message = "Please specify whether to deliver to registered address")
+                              Boolean deliverToRegisteredAddress,
+
+                              @Size(max = 500, message = "Shipping address must be under 500 characters")
+                              String shippingAddress) {
 
 }
