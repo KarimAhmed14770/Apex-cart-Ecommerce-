d6e3e43ffffa97c0f7003a52cca5c7b31fe5d6fd
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-28 10:13:54 +0300

    refactor:  payment_records table structure in the db

--- src/main/resources/db/migration/V2_Create_payment_records.sql
+++ src/main/resources/db/migration/V2_Create_payment_records.sql
@@ -1,17 +1,20 @@
-
+USE `Kee_V2C_Platform`;
 
 
 
 CREATE TABLE `payment_records` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`order_id` int NOT NULL,
-    `idempotency_key` varchar(36) NOT NULL,-- could be the same as the users address or different
-    `status` varchar(20) default 'PENDING',  -- FAILED,SUCCESS,PENDING
     `amount` DECIMAL(10,2) NOT NULL,
-    `transaction_reference` varchar(255), 
-    `processed_at` datetime default current_timestamp,
+    `payment_method` varchar(50) NOT NULL,
+    `transaction_reference` varchar(255),
+	`created_at` datetime default current_timestamp,
+    `processed_at` datetime default null,
+    `idempotency_key` varchar(36) ,
+    `status` varchar(20) default 'PENDING',  -- FAILED,SUCCESS,PENDING
 	PRIMARY KEY (`id`),
     Constraint `payment_record_order_fk` Foreign key (`order_id`) REFERENCES orders(`id`),
 	UNIQUE INDEX idx_unique_idempotency_key (idempotency_key)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
 
