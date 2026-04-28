bb665fb5d5265eefc73eca2d594e30d90da11894
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-28 08:56:09 +0300

    feat: add BadPaymentInfo exception for bad payment requests

--- src/main/java/com/Kee/V2C/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Kee/V2C/exception/GlobalExceptionHandler.java
@@ -218,6 +218,17 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException
+            (BadPaymentInfo badPaymentInfo){
+        UserErrorResponse error=new UserErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(badPaymentInfo.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
     public ResponseEntity<UserErrorResponse> handleException(Exception exc) {
         UserErrorResponse error = new UserErrorResponse();
         error.setStatus(HttpStatus.BAD_REQUEST.value());
