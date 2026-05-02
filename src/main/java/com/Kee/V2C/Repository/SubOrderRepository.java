81048b66d769e7fe7f67984fa2869c688e124a4a
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-05-02 14:08:37 +0300

    feat: add find suborder by vendor id, and another method with added filters

--- src/main/java/com/Kee/V2C/Repository/SubOrderRepository.java
+++ src/main/java/com/Kee/V2C/Repository/SubOrderRepository.java
@@ -2,8 +2,12 @@ package com.Kee.V2C.Repository;
 
 import com.Kee.V2C.entity.Order;
 import com.Kee.V2C.entity.SubOrder;
+import com.Kee.V2C.enums.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +15,28 @@ public interface SubOrderRepository extends JpaRepository<SubOrder,Long> {
     @Query(value="SELECT o FROM SubOrder o " +
             "JOIN FETCH o.orderItems " +
             "WHERE o.id= :id")
-    Optional<Order> findByIdWithItemsDetails(Long id);
+    Optional<SubOrder> findByIdWithItemsDetails(Long id);
+
+    @Query(value ="SELECT o FROM SubOrder o " +
+            "JOIN FETCH o.orderItems oi " +
+            "Join fetch oi.product "+
+            "WHERE o.vendor.id= :id "+
+            "ORDER BY o.orderedAt DESC",
+            countQuery = "SELECT COUNT(o) FROM SubOrder o " +
+                    "WHERE o.vendor.id = :id")
+    Page<SubOrder> findByVendorId(@Param("id")Long id, Pageable page);
+
+    @Query(value="SELECT o FROM SubOrder o "+
+            "JOIN FETCH o.orderItems oi " +
+            "Join fetch oi.product "+
+            "WHERE o.vendor.id = :vendorId " +
+            "AND (:status IS NULL OR o.status = :status) " +
+            "AND (:id IS NULL OR o.id = :id) "+
+            "ORDER BY o.orderedAt DESC",
+            countQuery = "SELECT COUNT(o) FROM SubOrder o " +
+                    "WHERE o.vendor.id = :vendorId " +
+                    "AND (:status IS NULL OR o.status = :status) " +
+                    "AND (:id IS NULL OR o.id = :id)")
+    Page<SubOrder> findByVendorIdWithFilters(@Param("vendorId")Long vendorId, @Param("status")OrderStatus status,
+            @Param("id")Long id, Pageable pageable);
 }
