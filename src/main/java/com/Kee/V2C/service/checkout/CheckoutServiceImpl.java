813fbb357fb4ac568bfc87611e33cb34d06459a1
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-05-03 10:36:06 +0300

    feat: add events publishing in the checkout service

--- src/main/java/com/Kee/V2C/service/checkout/CheckoutServiceImpl.java
+++ src/main/java/com/Kee/V2C/service/checkout/CheckoutServiceImpl.java
@@ -4,11 +4,13 @@ package com.Kee.V2C.service.checkout;
 import com.Kee.V2C.dto.checkout.CheckOutRequest;
 import com.Kee.V2C.dto.checkout.CheckoutResponse;
 import com.Kee.V2C.entity.*;
+import com.Kee.V2C.events.OrderPlacedEvent;
 import com.Kee.V2C.exception.*;
 import com.Kee.V2C.service.cart.CartService;
 import com.Kee.V2C.service.order.OrderService;
 import com.Kee.V2C.service.payment.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +21,14 @@ public class CheckoutServiceImpl implements CheckoutService{
     private final OrderService orderService;
     private final PaymentService paymentService;
     private final CartService cartService;
+    private final ApplicationEventPublisher applicationEventPublisher;
     @Autowired
     public CheckoutServiceImpl(OrderService orderService,PaymentService paymentService,
-                               CartService cartService){
+                               CartService cartService,ApplicationEventPublisher applicationEventPublisher){
         this.orderService=orderService;
         this.paymentService=paymentService;
         this.cartService=cartService;
+        this.applicationEventPublisher=applicationEventPublisher;
     }
 
     @Override
@@ -33,10 +37,15 @@ public class CheckoutServiceImpl implements CheckoutService{
         List<CartItem> cart=cartService.getCustomerCart(); //Retrieve: Fetch the Cart from the database using the userId.
         cartService.cartStockValidationAndUpdate(cart); //Validate: Check if every item in that cart is still in stock (The Atomic Shield).
         Order order=orderService.convertCartToOrder(checkOutRequest,cart);//Convert: Transform the Cart items into Order items and Order, persists order in the db
+        List<SubOrder> subOrders=order.getSubOrders();
         //process payment
         if(paymentService.processPayment(checkOutRequest.paymentRequest(),order)) {//payment successful
             cartService.deleteCart(cart);
-            //future features can be added here:notify each vendor with his subOrder
+            //publish an event for each suborder placed
+            for(SubOrder subOrder:subOrders){
+                applicationEventPublisher.publishEvent(new OrderPlacedEvent(subOrder.getId(),subOrder.getVendor().getId(),
+                        subOrder.getOrderedAt()));
+            }
             return new CheckoutResponse(order.getId(), order.getTotalPrice(), order.getStatus().name(),  //Respond: Return an OrderResponse.
                     order.getOrderedAt(), order.getShippingAddress());
         }
